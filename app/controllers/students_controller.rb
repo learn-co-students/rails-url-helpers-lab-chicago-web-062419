9fd2af59dbf46462b587a93d72bcbaa58201e9bc
srwatlanta <shawnrwheeler@gmail.com>
2019-07-19 16:18:22 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,49 @@
+require "pry"
+
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    set_student
   end
+
+  def edit
+    set_student
+  end
+
+  def activate
+    set_student
+
+    if @student.active == true
+      @student.active = false
+      @student.save
+      p "I am false"
+    else
+      @student.active = true
+      @student.save
+      p "I am true"
+    end
+    redirect_to student_path(@student)
+  end
+
+  # def activate_student
+  #   set_student
+  #   @student.update(params[:active])
+  #   redirect student(@student)
+  # end
+
+  # def update
+  #   set_student
+  #   @student.update(params[:active])
+  # end
 
   private
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
+  def set_student
+    @student = Student.find(params[:id])
+  end
 end
--- spec/features/student_spec.rb
+++ spec/features/student_spec.rb
@@ -1,15 +1,15 @@
-require 'byebug'
-require 'rails_helper'
+require "byebug"
+require "rails_helper"
 
-describe 'Route to view' do
-  it 'has an index page' do
+describe "Route to view" do
+  it "has an index page" do
     visit students_path
     expect(page.status_code).to eq(200)
   end
 end
 
-describe 'Multiple students' do
-  it 'shows them on the index page' do
+describe "Multiple students" do
+  it "shows them on the index page" do
     Student.create!(first_name: "Daenerys", last_name: "Targaryen")
     Student.create!(first_name: "Lindsey", last_name: "Stirling")
 
@@ -18,32 +18,32 @@ describe 'Multiple students' do
   end
 end
 
-describe 'Show page' do
+describe "Show page" do
   before do
     @student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
   end
 
-  it 'renders properly' do
+  it "renders properly" do
     visit student_path(@student)
     expect(page.status_code).to eq(200)
   end
 
-  it 'renders the first name in a h1 tag' do
+  it "renders the first name in a h1 tag" do
     visit student_path(@student)
     expect(page).to have_css("h1", text: "Daenerys")
   end
 
-  it 'renders the last name in a h1 tag' do
+  it "renders the last name in a h1 tag" do
     visit student_path(@student)
     expect(page).to have_css("h1", text: "Targaryen")
   end
 
-  it 'renders the active status if the user is inactive' do
+  it "renders the active status if the user is inactive" do
     visit student_path(@student)
     expect(page).to have_content("This student is currently inactive.")
   end
 
-  it 'renders the active status if the user is active' do
+  it "renders the active status if the user is active" do
     @student.active = true
     @student.save
     visit student_path(@student)
@@ -51,13 +51,13 @@ describe 'Show page' do
   end
 end
 
-describe 'Activate page' do
+describe "Activate page" do
   before do
     @student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
   end
 
   it "Should mark an inactive student as active" do
-    visit activate_student_path(@student)
+    visit activate_path(@student)
     @student.reload
     expect(@student.active).to eq(true)
   end
@@ -65,7 +65,7 @@ describe 'Activate page' do
   it "Should mark an active student as inactive" do
     @student.active = true
     @student.save
-    visit activate_student_path(@student)
+    visit activate_path(@student)
     @student.reload
     expect(@student.active).to eq(false)
   end
@@ -76,8 +76,8 @@ describe 'Activate page' do
   end
 end
 
-describe 'linking from the index page to the show page' do
-  it 'index page links to post page' do
+describe "linking from the index page to the show page" do
+  it "index page links to post page" do
     @student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
     visit students_path
     expect(page).to have_link(@student.to_s, href: student_path(@student))
